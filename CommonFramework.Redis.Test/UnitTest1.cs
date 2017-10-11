1913b5487f5986a7595af9a63bf3a3bd8cff5be3
shengxupeng <shengxupeng@126.com>
2017-10-11 14:00:54 +0800

    新增redis测试用例

---
+++ CommonFramework.Redis.Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonFramework.CastleWindsor;
+using System.Reflection;
+using CommonFramework.Core.Dependency;
+using CommonFramework.Core.Configure;
+using CommonFramework.Core.EntityFramework;
+using CommonFramework.Core.Email;
+
+namespace CommonFramework.Redis.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RedisTest1()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            if (IocContainer.Instance.Kernel.HasComponent(typeof(IDependencyProvider)))
+                return;
+            //CastleProvider.RegisterCastle();
+            //CastleProvider.Register(Assembly.GetExecutingAssembly(), typeof(IBaseDependency));
+            //CastleProvider.Register(Assembly.GetAssembly(typeof(IRedisConfiguration)), typeof(IBaseDependency));
+            CastleBuilderExtensions.AddCastleWindsor();
+            var app = IocContainer.Instance.Resolve<IAppBuilder>();
+            
+            app.AddAssembly<IBaseDependency>(Assembly.GetExecutingAssembly());
+            app.AddAssembly<IBaseDependency>(Assembly.GetAssembly(typeof(IRedisConfiguration)));
+            app.AddEfService()
+                .SetConnectionStringProvider(m => ConnectionStringProviderExtensions.GetWebConfigConnectionString(m), "testdatabaseEntities");
+            app.AddEmailService()
+                .Config(cfg => cfg.setHost("smtp.126.com").setSenderAddress("shengxupeng@126.com").setEmailSenderName("shengxupeng").setEmailPwd("999").setKey("aaa"))
+                .Config(cfg => cfg.setHost("smtp.exmail.qq.com").setSenderAddress("991823949@qq.com").setEmailSenderName("shengxupeng").setEmailPwd("9999").setKey("bbb").isDefault());
+            app.AddRedisService(IocContainer.Instance.Resolve<IRedisConfiguration>())
+                .AddEndPoints("127.0.0.1")
+                .Password("123456");
+        }
+    }
+}
